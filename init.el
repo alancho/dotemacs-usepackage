e145de38e21e1ade4865e706b345dd52ae009aa8
Alan Severini <alancho@debian>
2020-12-07 14:53:48 -0300

    Mejorar projectile autodiscover

--- init.el
+++ init.el
@@ -168,8 +168,9 @@
   :ensure t
   :bind-keymap ("C-c p" . projectile-command-map)
   :config
-  (setq projectile-completion-system 'ivy)
-  (setq projectile-enable-caching t)
+  (setq projectile-completion-system 'ivy
+	projectile-project-search-path '("~/Dropbox/")
+	projectile-enable-caching t)
   (projectile-mode))
 
 (use-package writeroom-mode
